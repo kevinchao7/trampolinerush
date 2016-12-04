245720edf8262be54d833a3fbfaa120be4336f2b
kevin chao <kchao562@gmail.com>
2016-12-03 21:55:38 -0800

    Add files via upload

---
+++ ios/src/com/machinagames/trampolinerushandroid/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.machinagames.trampolinerushandroid;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.machinagames.trampolinerushandroid.TrampolineRush;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new TrampolineRush(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
